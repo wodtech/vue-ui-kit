3fb0360aa2d96ad4261081c38d6f603dfadc204e
Vladislav Manoly <vladislav.manoly@gmail.com>
2022-06-23 12:23:13 +0300

    new icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -12,26 +12,27 @@ import IconQuality from "@/components/common/icons/IconQuality.vue";
 import IconWOD from "@/components/common/icons/IconWOD.vue";
 import IconXp from "@/components/common/icons/IconXp.vue";
 
-import IconFishingMap from '@/components/common/icons/menu/IconFishingMap';
-import IconCollection from '@/components/common/icons/menu/IconCollection';
-import IconMarketplace from '@/components/common/icons/menu/IconMarketplace';
-import IconCrafting from '@/components/common/icons/menu/IconCrafting';
-import IconGuild from '@/components/common/icons/menu/IconGuild';
-import IconRanking from '@/components/common/icons/menu/IconRanking';
-import IconEvents from '@/components/common/icons/menu/IconEvents';
-import IconAchievements from '@/components/common/icons/menu/IconAchievements';
+import IconFishingMap from "@/components/common/icons/menu/IconFishingMap";
+import IconCollection from "@/components/common/icons/menu/IconCollection";
+import IconMarketplace from "@/components/common/icons/menu/IconMarketplace";
+import IconCrafting from "@/components/common/icons/menu/IconCrafting";
+import IconGuild from "@/components/common/icons/menu/IconGuild";
+import IconRanking from "@/components/common/icons/menu/IconRanking";
+import IconEvents from "@/components/common/icons/menu/IconEvents";
+import IconAchievements from "@/components/common/icons/menu/IconAchievements";
 import IconWorkbench from "@/components/common/icons/menu/IconWorkbench";
+import IconBay from "@/components/common/icons/menu/IconBay";
+import IconPortal from "@/components/common/icons/menu/IconPortal";
 
 import IconCraft from "@/components/common/icons/IconCraft";
 import IconDestroy from "@/components/common/icons/IconDestroy";
 import IconRepair from "@/components/common/icons/IconRepair";
 import IconHooks from "@/components/common/icons/IconHooks";
 
-
 import IconAvatar from "@/components/common/icons/IconAvatar.vue";
 import IconBox from "@/components/common/icons/IconBox";
-import IconW  from "@/components/common/icons/IconW"
-import IconRod from "@/components/common/icons/IconRod"
+import IconW from "@/components/common/icons/IconW";
+import IconRod from "@/components/common/icons/IconRod";
 import IconEmptyData from "@/components/common/icons/IconEmptyData";
 
 import IconFloat from "@/components/common/icons/fishing-session/IconFloat";
@@ -42,12 +43,13 @@ import IconRodGear from "@/components/common/icons/fishing-session/IconRod";
 import IconSlotTooltip from "@/components/common/icons/fishing-session/IconSlotTooltip";
 import EditIcon from "@/components/common/icons/IconEdit";
 import IconMaterial from "@/components/common/icons/IconMaterial";
-
-import ClearIcon from "@/components/common/icons/ui-icons/TextFieldClear"
+import IconChange from "@/components/common/icons/IconChange";
+import ClearIcon from "@/components/common/icons/ui-icons/TextFieldClear";
+import IconPending from "@/components/common/icons/IconPending";
 
 Vue.use(Vuetify);
 
-Vuetify.config.silent = true
+Vuetify.config.silent = true;
 
 export default new Vuetify({
   icons: {
@@ -82,7 +84,7 @@ export default new Vuetify({
       xp: {
         component: IconXp,
       },
-      'fishing-map': {
+      "fishing-map": {
         component: IconFishingMap,
       },
       collections: {
@@ -110,59 +112,71 @@ export default new Vuetify({
         component: IconAchievements,
       },
       avatar: {
-        component: IconAvatar
+        component: IconAvatar,
       },
       box: {
-        component: IconBox
+        component: IconBox,
       },
-      'w-coin': {
-        component: IconW
+      "w-coin": {
+        component: IconW,
       },
-      'rod-fishing': {
-        component: IconRod
+      "rod-fishing": {
+        component: IconRod,
       },
       empty: {
-        component: IconEmptyData
+        component: IconEmptyData,
       },
-      'rod': {
-        component: IconRodGear
+      rod: {
+        component: IconRodGear,
       },
       float: {
-        component: IconFloat
+        component: IconFloat,
       },
       hook: {
-        component: IconHook
+        component: IconHook,
       },
       line: {
-        component: IconLine
+        component: IconLine,
       },
       reel: {
-        component: IconReel
+        component: IconReel,
       },
-      'slot-tooltip': {
-        component: IconSlotTooltip
+      "slot-tooltip": {
+        component: IconSlotTooltip,
       },
       edit: {
-        component: EditIcon
+        component: EditIcon,
       },
       repair: {
-        component: IconRepair
+        component: IconRepair,
       },
       craft: {
-        component: IconCraft
+        component: IconCraft,
       },
       destroy: {
-        component: IconDestroy
+        component: IconDestroy,
       },
       hooks: {
-        component: IconHooks
+        component: IconHooks,
       },
       material: {
-        component: IconMaterial
+        component: IconMaterial,
       },
-      'clear-icon': {
-        component: ClearIcon
-      }
+      "clear-icon": {
+        component: ClearIcon,
+      },
+      portal: {
+        component: IconPortal,
+      },
+      bay: {
+        component: IconBay,
+      },
+      change: {
+        component: IconChange,
+      },
+      pending: {
+        component: IconPending,
+      },
     },
   },
   theme: {
@@ -184,17 +198,17 @@ export default new Vuetify({
         secondary_new: "#85C0F0",
         info: "#408CFF",
         blue: "#41ABE1",
-        dark_green: '#2F5664',
-        gray: '#A8A5C6',
-        main_bg: '#06101C',
-        rd_slot: '#21486C',
-        rd_blue_light: '#85C0F0',
-        'rarity-common': "#A0A2C1",
-        'rarity-uncommon': "#ffffff",
-        'rarity-rare': "#41ABE1",
-        'rarity-epic': "#C463FF",
-        'rarity-legendary': "#FEB11D",
-        'rarity-artifact': "#E11900",
+        dark_green: "#2F5664",
+        gray: "#A8A5C6",
+        main_bg: "#06101C",
+        rd_slot: "#21486C",
+        rd_blue_light: "#85C0F0",
+        "rarity-common": "#A0A2C1",
+        "rarity-uncommon": "#ffffff",
+        "rarity-rare": "#41ABE1",
+        "rarity-epic": "#C463FF",
+        "rarity-legendary": "#FEB11D",
+        "rarity-artifact": "#E11900",
       },
     },
   },
